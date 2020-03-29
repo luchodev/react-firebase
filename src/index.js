6919dd74ac41acf7f786956fdb96149e8e1a666d
Luis Ruiz <luis20164@hotmail.es>
2020-03-29 11:44:41 -0500

    Config file

--- src/index.js
+++ src/index.js
@@ -3,17 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import firebase from "firebase";
+import config from "./config";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyDs0H7SvIXc0G24LYUMDzrBOUSwq3GmuPk",
-  authDomain: "pseudogram-d590d.firebaseapp.com",
-  databaseURL: "https://pseudogram-d590d.firebaseio.com",
-  projectId: "pseudogram-d590d",
-  storageBucket: "pseudogram-d590d.appspot.com",
-  messagingSenderId: "752284085527",
-  appId: "1:752284085527:web:6658b65af14027f0b61b52"
-});
+config.initializeFirebase();
 
 ReactDOM.render(
   <React.StrictMode>
